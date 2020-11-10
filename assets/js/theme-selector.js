1def892d3c68b4f9cd795d0e767c03ed253ac06d
Sandeep Mukherjee <sandeep.m@berkeley.edu>
2020-11-10 14:52:45 -0500

    finished toggle button js

--- assets/js/theme-selector.js
+++ assets/js/theme-selector.js
@@ -1,22 +1,53 @@
 const btn = document.querySelector(".toggle");
 const theme = document.getElementById("theme");
-document.cookie = "isDark = false"
 // Listen for a click on the button
 btn.addEventListener("click", changeTheme);
 
-function changeTheme() {
-    // If the current URL contains "ligh-theme.css"
-    if (theme.getAttribute("href") == "./assets/css/stylelight.css") {
-      // ... then switch it to "dark-theme.css"
-      theme.href = "./assets/css/styledark.css";
-    // Otherwise...
+/* direction = -1 for going from dark to light
+else = 1
+*/
+function switchMove(direction) {
+  var elem = document.getElementById("switch");   
+  var pos = 5;
+  var end = 45
+  backwards = direction < 0;
+  if (backwards) {
+    pos = 45;
+    end = 5;
+  }
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (pos == end) {
+      clearInterval(id);
     } else {
-      // ... switch it to "light-theme.css"
-      theme.href = "./assets/css/stylelight.css";
+      if (backwards) {
+        pos--; 
+        elem.style.left = pos + 'px'; 
+      }
+      else {
+        pos++; 
+        elem.style.left = pos + 'px'; 
+      }
     }
+  }
 }
 
-// window.onload(function () {
-//     var dark = document.cookie;
+function changeTheme() {
+  if (theme.getAttribute("href") == "./assets/css/stylelight.css") {
+    switchMove(1)
+    theme.href = "./assets/css/styledark.css";
+    writeCookie(false);
+  } else {
+    switchMove(-1)
+    theme.href = "./assets/css/stylelight.css";
+    writeCookie(true);
+  }
+}
+function writeCookie(isDark) {
+  cookievalue = isDark + ";";
+  document.cookie = "darkMode=" + cookievalue;
+}
 
-// });
+function readCookie() {
+  document.write(document.cookie);
+}
