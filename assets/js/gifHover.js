b5adb1236fec7b90a97a8af6eee7956cb6ce2ba4
Sandeep Mukherjee <sandeep.m@berkeley.edu>
2020-11-11 16:24:18 -0500

    added content formatting

--- assets/js/gifHover.js
+++ assets/js/gifHover.js
@@ -7,7 +7,10 @@ var imgs = document.querySelectorAll(".proj-container-content img");
 imgs.forEach(img => {
     const pathBuilder = img.src.search("assets");
     const path = "./" + img.src.slice(pathBuilder);
+    console.log(path, img.id);
+    console.log(document.getElementById(img.id));
     document.getElementById(img.id).onclick = function() {playGif(path, img.id);};
+    console.log(document.getElementById(img.id).onclick);
   })
 
 
@@ -15,5 +18,6 @@ function playGif(imgName, elemID) {
     const elem = document.getElementById(elemID);
     if (gifMap.has(imgName)) {
         elem.src = gifMap.get(imgName);
+        console.log("swapped");
     }
 }
