573b08b8209767e4e83360f9f55f5b6cf2f8b831
Sandeep Mukherjee <sandeep.m@berkeley.edu>
2020-11-12 19:30:14 -0500

    gif thingy working, but ugly

--- assets/js/gifHover.js
+++ assets/js/gifHover.js
@@ -1,23 +1,37 @@
 // Initializing map of gifs
 let gifMap = new Map();
 gifMap.set("./assets/img/BearMaps.JPG", "./assets/img/BearMapsGif.gif");
+gifMap.set("./assets/img/NBodyExtreme.PNG","./assets/img/NBodyExtremeGif.gif")
+gifMap.set("./assets/img/Scheme.PNG", "./assets/img/SchemeGif.gif")
+gifMap.set("./assets/img/Cats.PNG", "./assets/img/CatsGif.gif")
 
-// Adding event listener
-var imgs = document.querySelectorAll(".proj-container-content img");
+
+imgs = document.querySelectorAll(".proj-container-content img");
 imgs.forEach(img => {
-    const pathBuilder = img.src.search("assets");
-    const path = "./" + img.src.slice(pathBuilder);
-    console.log(path, img.id);
-    console.log(document.getElementById(img.id));
-    document.getElementById(img.id).onclick = function() {playGif(path, img.id);};
-    console.log(document.getElementById(img.id).onclick);
-  })
+    document.getElementById(img.id).addEventListener("mouseenter", function() {playGif(img.src,img.id)});
+    document.getElementById(img.id).addEventListener("mouseleave", function() {playGif(gifMap.get(img.src),img.id)});
+    console.log(img.id + " " + img.src);
+});
+
+var bearmaps = document.getElementById("bearmaps");
+bearmaps.addEventListener("mouseenter", function() {playGif("./assets/img/BearMaps.JPG","bearmaps")});
+bearmaps.addEventListener("mouseleave", function() {playGif(gifMap.get("./assets/img/BearMaps.JPG"),"bearmaps")});
+var nbody = document.getElementById("Nbody");
+nbody.addEventListener("mouseenter", function() {playGif("./assets/img/NBodyExtreme.PNG","Nbody")});
+nbody.addEventListener("mouseleave", function() {playGif(gifMap.get("./assets/img/NBodyExtreme.PNG"),"Nbody")});
+var scheme = document.getElementById("scheme");
+scheme.addEventListener("mouseenter", function() {playGif("./assets/img/Scheme.PNG","scheme")});
+scheme.addEventListener("mouseleave", function() {playGif(gifMap.get("./assets/img/Scheme.PNG"),"scheme")});
+var cats = document.getElementById("cats");
+cats.addEventListener("mouseenter", function() {playGif("./assets/img/Cats.PNG","cats")});
+cats.addEventListener("mouseleave", function() {playGif(gifMap.get("./assets/img/Cats.PNG"),"cats")});
 
 
 function playGif(imgName, elemID) {
     const elem = document.getElementById(elemID);
     if (gifMap.has(imgName)) {
         elem.src = gifMap.get(imgName);
+        gifMap.set(gifMap.get(imgName), imgName)
         console.log("swapped");
     }
 }
